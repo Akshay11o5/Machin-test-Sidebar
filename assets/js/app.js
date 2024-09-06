ce1283d14c6dfdff4508c44a91740302b2513e84
Akshay11o5 <thoratakshay8230@gmail.com>
2024-09-06 14:42:52 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+cl = console.log;
+
+const toggleSidebar = [...document.querySelectorAll(".togglesidebar")];
+
+const backdrop = document.getElementById("backdrop");
+
+const sideBar = document.getElementById("sidebar");
+
+const onNavToggle = () => {
+  sideBar.classList.toggle("active");
+  backdrop.classList.toggle("active");
+};
+
+toggleSidebar.forEach((ele) => {
+  ele.addEventListener("click", onNavToggle);
+});
